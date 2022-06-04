d5dc6cc33c8f8b16015a7cad73d41198a2ba4338
Alfred Cepeda <alfred@cepeda.io>
2022-06-03 23:59:48 -0400

    Fixes how cli is installed

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,7 @@
+#!/usr/bin/env node
+import { buildProgram } from './utils/register';
+import { rootDir } from './config/app';
 import {init} from "./scripts/init";
-import {buildProgram} from "./utils/register";
-import {rootDir} from "./config/app";
 
 init().then(() => {
   const program = buildProgram(`${rootDir}/commands`, ['shell', 'ws', 'vue']);
